72c3727b9ccbf2935b7337f7090282d9c3f2caf0
Felipe Ivanaga <felipeivanaga@pm.me>
2022-06-29 09:54:13 -0300

    ensure min and max length of name

--- domain/entity/User/User.spec.ts
+++ domain/entity/User/User.spec.ts
@@ -1,12 +1,23 @@
-import { MissingParamException } from "@application/exception/MissingParamException";
+import { InvalidParamException } from "@application/exception/InvalidParamException";
 import { User } from "./User";
 
 describe("User entity", () => {
     describe("create", () => {
-        it("should throw when name is invalid", () => {
+        it("should throw when name length < 3", () => {
+            const err = new InvalidParamException(
+                "name must have a length between 3 and 32"
+            );
             expect(() => {
-                new User("");
-            }).toThrow(MissingParamException);
+                new User("aa");
+            }).toThrow(err);
+        });
+        it("should throw when name length > 32", () => {
+            const err = new InvalidParamException(
+                "name must have a length between 3 and 32"
+            );
+            expect(() => {
+                new User("xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx");
+            }).toThrow(err);
         });
     });
 });
--- domain/entity/User/User.ts
+++ domain/entity/User/User.ts
@@ -1,10 +1,18 @@
-import { MissingParamException } from "@application/exception/MissingParamException";
+import { InvalidParamException } from "@application/exception/InvalidParamException";
 
 export class User {
     private name: string;
 
     constructor(name: string) {
-        if (!name) throw new MissingParamException("name");
+        if (name.length < 3 || name.length > 32) {
+            throw new InvalidParamException(
+                "name must have a length between 3 and 32"
+            );
+        }
+
+        if (!RegExp("[a-zA-Z]").test(name)) {
+            throw new Error("teste");
+        }
         this.name = name;
     }
 }
